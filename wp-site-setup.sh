9e4dae24c5c1c447fca72ffdca780550e5c1a976
Johan N. Martin <jnm+johanmartindev@catenare.com>
2023-11-02 17:02:55 +0000

    Update to the config

--- wp-site-setup.sh
+++ wp-site-setup.sh
@@ -7,7 +7,7 @@ composer install
 wp --info --allow-root
 wp db reset --yes
 
-wp core install --url=${DOMAIN_CURRENT_SITE} --title="${SITE_TITLE}" --admin_user=admin --admin_password=randomtest --admin_email=info@johanmartin.dev --debug
+wp core install --admin_user=admin --admin_password=randomtest --admin_email=info@johanmartin.dev --debug
 
 # wp plugin activate woocommerce
 wp core update-db
