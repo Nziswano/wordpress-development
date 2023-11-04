ee16402588e9473e6acd5337da34984ceed7a06b
Johan N. Martin <jnm+johanmartindev@catenare.com>
2023-11-04 13:33:53 +0000

    Add link to site setup

--- wp-site-setup.sh
+++ wp-site-setup.sh
@@ -10,3 +10,8 @@ wp db reset --yes
 wp core install --url=${DOMAIN_CURRENT_SITE} --title="${SITE_TITLE}" --admin_user=admin --admin_password=randomtest --admin_email=info@johanmartin.dev --debug
 
 wp core update-db
+
+# Setup plugin
+ln -s /workspace/docfox-plugin /workspace/wordpress-development/wordpress/wp-con
+tent/plugins
+
