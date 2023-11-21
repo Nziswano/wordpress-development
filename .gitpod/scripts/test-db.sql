c9acde95ca6b9a26173a18fe7b8078e04ae3b599
Johan N. Martin <jnm+johanmartindev@catenare.com>
2023-11-21 16:14:06 +0000

    Update setup of databases

--- .gitpod/scripts/test-db.sql
+++ .gitpod/scripts/test-db.sql
@@ -3,6 +3,5 @@
  *
  * SQL queries in this file will be executed the first time the MySQL server is started.
  */
-
-CREATE DATABASE IF NOT EXISTS wordpress_develop;
 CREATE DATABASE IF NOT EXISTS wordpress_develop_tests;
+GRANT ALL PRIVILEGES ON *.* TO 'wordpress'@'%' IDENTIFIED BY 'wordpress';
