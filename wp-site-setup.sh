353974f9003d8c4d648323e4bc7e6b6c4c1a2405
Johan Martin <jnm+johanmartindev@catenare.com>
2023-12-02 14:44:44 +0000

    Make setup script smarter

--- wp-site-setup.sh
+++ wp-site-setup.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
+
+# is wordpress installed?
+if wp core is-installed; then
+  echo "WordPress is already installed."
+  exit 0
+fi
+
 cp example.env .env
 set -a; source .env; set +a
 composer install
-# is wordpress installed?
-
 wp db reset --yes
-
 wp core install --url=${DOMAIN_CURRENT_SITE} --title="${SITE_TITLE}" --admin_user=admin --admin_password=randomtest --admin_email=info@johanmartin.dev --debug
-
 wp plugin install --activate https://github.com/10up/wpcli-vulnerability-scanner/archive/stable.zip
 wp core update-db
-
 # Setup plugin
 ln -s /workspace/docfox-plugin /workspace/wordpress-development/wordpress/wp-content/plugins
+
+
