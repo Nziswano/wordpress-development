d1c1b32620742cb47f0708556c8a6fdd38ac84d8
Johan Martin <jnm+johanmartindev@catenare.com>
2023-12-03 07:25:02 +0000

    Remove unnecessary plugins

--- wp-site-setup.sh
+++ wp-site-setup.sh
@@ -10,7 +10,6 @@ else
 	composer install
 	wp db reset --yes
 	wp core install --url=${DOMAIN_CURRENT_SITE} --title="${SITE_TITLE}" --admin_user=admin --admin_password=randomtest --admin_email=info@johanmartin.dev --debug
-	wp plugin install --activate https://github.com/10up/wpcli-vulnerability-scanner/archive/stable.zip
 	wp core update-db
 	# Setup plugin
 	ln -s /workspace/docfox-plugin /workspace/wordpress-development/wordpress/wp-content/plugins
