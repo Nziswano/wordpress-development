bd4b686c0fd871cc352c41edc5c251cf09fed237
Johan Martin <jnm+johanmartindev@catenare.com>
2023-12-14 06:00:23 +0000

    Update config

--- wp-site-setup.sh
+++ wp-site-setup.sh
@@ -3,14 +3,19 @@ set -Eeuo pipefail
 
 # is wordpress installed?
 if wp core is-installed; then
-  echo "WordPress is already installed."
+	echo "WordPress is already installed."
 else
 	cp example.env .env
-	set -a; source .env; set +a
+	set -a
+	source .env
+	set +a
 	composer install
 	wp db reset --yes
 	wp core install --url=${DOMAIN_CURRENT_SITE} --title="${SITE_TITLE}" --admin_user=admin --admin_password=randomtest --admin_email=info@johanmartin.dev --debug
 	wp core update-db
+# Setup plugin
+ln -s /workspace/docfox-contact-user /workspace/wordpress-development/wordpress/wp-content/plugins/docfox-contact-user
 fi
+
 # start server
 wp server --port=8000 --debug --color --host=127.0.0.1
