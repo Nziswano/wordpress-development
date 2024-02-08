2e34cb7ec8a80b98140db2d24e108c85c7ba0751
Johan Martin <jnm+johanmartindev@catenare.com>
2024-02-08 10:29:36 +0000

    Add check to see if the directory exists

--- wp-site-setup.sh
+++ wp-site-setup.sh
@@ -14,6 +14,9 @@ else
 	wp core install --url=${DOMAIN_CURRENT_SITE} --title="${SITE_TITLE}" --admin_user=admin --admin_password=randomtest --admin_email=info@johanmartin.dev --debug
 	wp core update-db
 # Setup plugin
+PLUGIN_DIR=/workspace/wordpress-development/wordpress/wp-content/plugins/docfox-contact-user
+PLUGIN_FILE=/workspace/wordpress-development/wordpress/wp-content/plugins/docfox-contact-user/contact-user-plugin.php
+if [ ! -f "$PLUGIN_FILE" ]; then
 ln -s /workspace/docfox-contact-user /workspace/wordpress-development/wordpress/wp-content/plugins/docfox-contact-user
 fi
 
